8788bfaf44f7590ba35f0da448563194d708b150
Gustavo <gustavocop14dos@gmail.com>
2025-09-02 14:26:39 -0300

    connect php

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// the one can use php -a
+// inside the directory that has the vendor folder
+// just add the require and the other commands
+
+// apt install php8.1-mongodb -y
+// composer require mongodb/mongodb
+
+// script in a simple php file
+// used docker image gustavovinicius/gusphp:81
+
+$client = new MongoDB\Client('mongodb://root:example@mongo:27017');
+// admin is the db, linux is the collection
+$client->admin->oses->insertOne(["name" => "debian"]);
+$document = $client->admin->oses->findOne(['name' => 'debian']);
+print_r(json_encode($document));
